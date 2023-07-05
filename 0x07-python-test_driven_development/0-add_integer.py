6dede0fc00e3e776028ab00d0e8f8c657a389ed0
sabo550 <saborahmatu866@gmail.com>
2023-07-05 01:08:47 -0700

    updated

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/python3
 # 0-add_integer.py
 """Defines an integer addition function."""
@@ -16,5 +17,4 @@ def add_integer(a, b=98):
     if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
-
 
